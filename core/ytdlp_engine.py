edf1b2b760205246ec583826ea18bb4da4fb51b0
arshiacomplus <arshiacomplus@gmail.com>
2026-05-07 19:34:47 +0330

    fixed  yt-dlp

--- core/ytdlp_engine.py
+++ core/ytdlp_engine.py
@@ -37,9 +37,10 @@ async def download_media(url: str, quality: str, updater: ProgressUpdater, user_
             f.write(user_cookies.strip())
 
     async def run_ytdlp(use_cookies: bool) -> tuple[int, list[str]]:
+        YTDLP_PATH = "/usr/local/bin/yt-dlp" 
 
         cmd_parts = [
-            "yt-dlp", "--no-warnings", "--no-playlist",
+            YTDLP_PATH, "--no-warnings", "--no-playlist",
             "--user-agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36",
             "--extractor-args", "youtube:player_client=web"
         ]
